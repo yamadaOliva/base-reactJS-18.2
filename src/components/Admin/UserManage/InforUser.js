82d6e594789cb8ca3387d0652f8c1be1f80efb9f
unknown <Vinh.pn260701@gmail.com>
2023-07-02 20:35:30 +0700

    hide/show blocked user

--- src/components/Admin/UserManage/InforUser.js
+++ src/components/Admin/UserManage/InforUser.js
@@ -1,8 +1,31 @@
 import './InforUser.scss'
 import { FaEye } from 'react-icons/fa'
-import { IoIosSwitch } from 'react-icons/io'
+import Switch from "react-switch";
+import { useState } from 'react';
 
-const InforUser = () => {
+const InforUser = (props) => {
+
+    const { id, name, numberphone, address, active } = props;
+    const [checked, setChecked] = useState(active);
+
+    const handleChange = (checked) => {
+        // true la mau xanh
+        // false la mau do            
+        setChecked(checked);
+        props.updateActiveStatus(id, checked);
+        console.log("id: " + id + " checked: " + checked);
+        if (checked === true) {
+            props.setTotalBlock(props.totalBlock - 1);
+        } else {
+            props.setTotalBlock(props.totalBlock + 1);
+        }
+    }
+
+    // console.log("index: " + props.index);
+    // console.log("id: " + id);
+    // console.log("acive: " + active);
+    console.log("checked check: " + checked);
+
     return (
         <>
             <div className="infor-user">
@@ -10,14 +33,41 @@ const InforUser = () => {
                     <img src="https://images.squarespace-cdn.com/content/v1/594c9a79414fb5311da8888f/1500584272653-TM0X3I8VC5YGDT4VYHZ8/unknown+border.jpg?format=500w" alt="" />
                 </div>
                 <div className="infor-user__detail">
-
+                    <div className="infor-user__detail__Id__name">
+                        <div className="infor-user__detail__Id">
+                            <span>ID: </span>
+                            <span>{id}</span>
+                        </div>
+                        <div className="infor-user__detail__name">
+                            <span>名前: </span>
+                            <span>{name}</span>
+                        </div>
+                    </div>
+                    <div className="infor-user__detail__numberphone">
+                        <span>電話番号: </span>
+                        <span>{numberphone}</span>
+                    </div>
+                    <div className="infor-user__detail__address">
+                        <span>住所: </span>
+                        <span>{address}</span>
+                    </div>
                 </div>
                 <div className="infor-user__btn_block">
                     <div className="infor-user__btn__title">
-                        禁止
+                        状態
                     </div>
                     <div className="infor-user__btn__switch">
-                        <IoIosSwitch size={30} className='icon-switch' />
+                        <label>
+                            <Switch
+                                onChange={handleChange}
+                                checked={checked}
+                                checkedIcon={false}
+                                uncheckedIcon={false}
+                                offHandleColor='#F06E6E'
+                                onHandleColor='#71F06E'
+                                onColor='#888888'
+                            />
+                        </label>
                     </div>
                 </div>
                 <div className="infor-user__cancle__rate">
--- src/components/Admin/UserManage/UserManage.js
+++ src/components/Admin/UserManage/UserManage.js
@@ -3,8 +3,86 @@ import Header from '../../Header/Header'
 import InforUser from './InforUser'
 import './UserManage.scss'
 import { FaSearch } from 'react-icons/fa'
+import { useState } from 'react'
 
 const UserManage = () => {
+
+    const [listUser, setListUser] = useState([
+        {
+            id: 1,
+            name: 'Nguyen Van A',
+            numberphone: '090-1234-5678',
+            address: '東京都',
+            active: false
+        },
+        {
+            id: 2,
+            name: 'Nguyen Van B',
+            numberphone: '090-1234-5678',
+            address: '東京都',
+            active: true
+        },
+        {
+            id: 3,
+            name: 'Nguyen Van C',
+            numberphone: '090-1234-5678',
+            address: '東京都',
+            active: false
+        },
+        {
+            id: 4,
+            name: 'Nguyen Van D',
+            numberphone: '090-1234-5678',
+            address: '東京都',
+            active: false
+        },
+        {
+            id: 5,
+            name: 'Nguyen Van E',
+            numberphone: '090-1234-5678',
+            address: '東京都',
+            active: false
+        }
+    ])
+
+    const [isHideBlock, setIsHideBlock] = useState(false)
+
+    // set value of block
+    const updateActiveStatus = (id, checked) => {
+        setListUser(prevListUser => {
+            const updatedListUser = prevListUser.map(user => {
+                if (user.id === id) {
+                    return {
+                        ...user,
+                        active: checked
+                    };
+                }
+                return user;
+            });
+            return updatedListUser;
+        });
+    };
+
+    //onclick btn hide blocked user
+    const hideBlockedUser = () => {
+        setIsHideBlock(true);
+    }
+
+    const showBlockedUser = () => {
+        setIsHideBlock(false);
+    }
+
+
+    //useEffect  ListUser
+
+
+
+
+
+
+    const [totalUser, setTotalUser] = useState(listUser.length);
+    const [totalBlock, setTotalBlock] = useState(listUser.filter(user => user.active === false).length);
+
     return (
         <>
             <div className="container-usermanage">
@@ -21,32 +99,78 @@ const UserManage = () => {
                             </div>
                             <div className="search-user-table__infor">
                                 <div className="search-user-table__infor__detail">
-                                    総ユーザー数: 100
+                                    総ユーザー数: {totalUser}
                                 </div>
                                 <div className="search-user-table__infor__detail">
-                                    禁止されたユーザー: 10
+                                    禁止されたユーザー: {totalBlock}
                                 </div>
                             </div>
                             <div className="search-user-table__btn">
                                 <div className="search-user-table__btn__sort">
                                     IDで並べ替える
                                 </div>
-                                <div className="search-user-table__btn__sort">
+                                {/* <div className="search-user-table__btn__sort">
                                     時間順で並べ替える
-                                </div>
-                                <div className="search-user-table__btn__block">
-                                    禁止を隠す
-                                </div>
+                                </div> */}
+                                {/* check isHideBlock */}
+                                {
+                                    isHideBlock === false ?
+                                        <div
+                                            className="search-user-table__btn__block"
+                                            onClick={() => hideBlockedUser()}
+                                        >
+                                            禁止を隠す
+                                        </div>
+                                        :
+                                        <div
+                                            className="search-user-table__btn__block"
+                                            onClick={() => showBlockedUser()}
+                                        >
+                                            全部を表示する
+                                        </div>
+
+                                }
                             </div>
                         </div>
                     </div>
                     <div className="content-page__right">
                         <div className="user__manage__contain">
-                            <InforUser />
-                            <InforUser />
-                            <InforUser />
-                            <InforUser />
-                            <InforUser />
+                            {
+                                isHideBlock === false ?
+
+                                    listUser.map((user, index) => {
+                                        return (
+                                            <InforUser
+                                                index={index}
+                                                id={user.id}
+                                                name={user.name}
+                                                numberphone={user.numberphone}
+                                                address={user.address}
+                                                active={user.active}
+                                                updateActiveStatus={updateActiveStatus}
+                                                totalBlock={totalBlock}
+                                                setTotalBlock={setTotalBlock}
+                                            />
+                                        )
+                                    })
+                                    :
+                                    listUser.filter(user => user.active === true).map((user, index) => {
+                                        return (
+                                            <InforUser
+                                                index={index}
+                                                id={user.id}
+                                                name={user.name}
+                                                numberphone={user.numberphone}
+                                                address={user.address}
+                                                active={user.active}
+                                                updateActiveStatus={updateActiveStatus}
+                                                totalBlock={totalBlock}
+                                                setTotalBlock={setTotalBlock}
+                                            />
+                                        )
+                                    }
+                                    )
+                            }
                         </div>
                     </div>
                 </div>
