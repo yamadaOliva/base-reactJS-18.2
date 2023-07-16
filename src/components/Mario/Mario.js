849153419a54bab157c10fdaf63545b6e48a3593
kien2572001 <kien2572001.ntk@gmail.com>
2023-07-16 21:57:43 +0700

    feat: done

--- src/components/Mario/Mario.js
+++ src/components/Mario/Mario.js
@@ -18,6 +18,11 @@ const marioLocation = {
   x: 7,
   y: 3,
 };
+
+const diamondLocation = {
+  x: 0,
+  y: 4,
+};
 //define state of block
 // 0: empty
 // 1: wall (blue)
@@ -31,12 +36,17 @@ const marioLocation = {
 // 2: left
 // 3: right
 
-const initMove = [2, 2, 2, 2];
+const initMove = [2, 2, 0, 0, 0, 3, 3, 3, 0, 0, 2, 2, 0, 0, 3, 3];
 
 const Mario = () => {
   const [layout, setLayout] = useState(initLayout);
   const [move, setMove] = useState(initMove);
   const [mario, setMario] = useState(marioLocation);
+  const [diamond, setDiamond] = useState(diamondLocation);
+  const upBtnRef = useRef();
+  const downBtnRef = useRef();
+  const leftBtnRef = useRef();
+  const rightBtnRef = useRef();
 
   const renderMoveText = (move) => {
     switch (move) {
@@ -59,23 +69,27 @@ const Mario = () => {
 
   const handleStart = async () => {
     let moveIndex = 0;
-
     const executeNextMove = () => {
       if (moveIndex < move.length) {
-        const currentMove = move[moveIndex];
-        handleMove(currentMove);
+        let currentMove = move[moveIndex];
+        //handleMove(currentMove);
+        if (currentMove === 0) {
+          upBtnRef.current.click();
+        } else if (currentMove === 1) {
+          downBtnRef.current.click();
+        } else if (currentMove === 2) {
+          leftBtnRef.current.click();
+        } else if (currentMove === 3) {
+          rightBtnRef.current.click();
+        }
         moveIndex++;
-
-        setTimeout(executeNextMove, 1000); // Chờ 1 giây trước khi thực hiện lệnh tiếp theo
+        setTimeout(executeNextMove, 500); // Chờ 1 giây trước khi thực hiện lệnh tiếp theo
       }
     };
-
     executeNextMove();
   };
 
   const handleMove = (move) => {
-    console.log("move", move);
-    console.log("mario1", mario);
     let newMario = {
       ...mario,
     };
@@ -116,6 +130,10 @@ const Mario = () => {
       default:
         break;
     }
+    if (layout[newMario.x][newMario.y] === 1) {
+      //check collision
+      return;
+    }
     setMario((mario) => newMario);
   };
 
@@ -145,6 +163,13 @@ const Mario = () => {
         }
       });
     });
+
+    //check win
+    if (mario.x === diamond.x && mario.y === diamond.y) {
+      newLayout[mario.x][mario.y] = 4;
+      alert("You win");
+    }
+
     setLayout((layout) => newLayout);
   }, [mario]);
 
@@ -174,6 +199,26 @@ const Mario = () => {
                         />
                       </div>
                     );
+                  } else if (col === 3) {
+                    return (
+                      <div className="col col_customize flex">
+                        <img
+                          src={window.location.origin + "/diamond.png"}
+                          alt="mario"
+                          className="object-contain scale-150"
+                        />
+                      </div>
+                    );
+                  } else if (col === 4) {
+                    return (
+                      <div className="col col_customize flex">
+                        <img
+                          src={window.location.origin + "/win.png"}
+                          alt="mario"
+                          className="object-contain scale-150"
+                        />
+                      </div>
+                    );
                   } else {
                     return <div className="col col_customize"></div>;
                   }
@@ -192,11 +237,41 @@ const Mario = () => {
           ))}
         </div>
         <div className="Control-Container-Btn">
+          <div className="hidden	">
+            <button
+              className="Control-Container-Btn-Item btn-danger btn"
+              onClick={() => handleMove(0)}
+              ref={upBtnRef}
+            >
+              Up
+            </button>
+            <button
+              className="Control-Container-Btn-Item btn-danger btn"
+              onClick={() => handleMove(1)}
+              ref={downBtnRef}
+            >
+              Down
+            </button>
+            <button
+              className="Control-Container-Btn-Item btn-danger btn"
+              onClick={() => handleMove(2)}
+              ref={leftBtnRef}
+            >
+              Left
+            </button>
+            <button
+              className="Control-Container-Btn-Item btn-danger btn"
+              onClick={() => handleMove(3)}
+              ref={rightBtnRef}
+            >
+              Right
+            </button>
+          </div>
           <button
             className="Control-Container-Btn-Item btn-success btn"
             onClick={() => handleStart()}
           >
-            Start
+            Fake
           </button>
         </div>
       </div>
